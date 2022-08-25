22b9639b0e1c69a4a0dc7ad1e7ba55ca2f20c0a0
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 23:52:32 -0300

    [feat] requisito 7 search by date ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,11 @@
-# Requisito 6
+from datetime import datetime
 from tech_news.database import search_news
 
 
+# Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    # https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
     lis = search_news({"title": {"$regex": title, "$options": "i"}})
     return [(i["title"], i["url"]) for i in lis]
 
@@ -11,6 +13,13 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    lis = search_news({"timestamp": "/".join(date.split("-")[::-1])})
+    return [(i["title"], i["url"]) for i in lis]
 
 
 # Requisito 8
