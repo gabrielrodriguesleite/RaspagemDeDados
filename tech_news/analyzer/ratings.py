69f64e47f7dfa3b277acf620417b0b842dab038d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-25 00:22:05 -0300

    [feat] requisito 10 ranking pop 5 ok

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,12 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    ord_alp = sorted(find_news(), key=lambda x: x["title"])
+    ord_pop = sorted(ord_alp, key=lambda x: -x["comments_count"])
+    return [(i["title"], i["url"]) for i in ord_pop][:5]
 
 
 # Requisito 11
