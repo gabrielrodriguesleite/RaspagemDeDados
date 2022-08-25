ddf2336f5feecba32c7b3a6dcba4b40a19fc95a3
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 21:25:51 -0300

    [feat] requisito 6 search by title (regex) ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,11 @@
 # Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    lis = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(i["title"], i["url"]) for i in lis]
 
 
 # Requisito 7
